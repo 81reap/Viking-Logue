3cd6a6ab707e829f5030e5771aa8975401228b6d
Hax0rDoge <p.bhakar@gmail.com>
2016-08-03 05:45:43 -0500

    Initial commit

---
+++ Viking Logue/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Viking Logue
+//
+//  Created by Hax0rDoge on 8/2/16.
+//  Copyright © 2016 Hax0rDoge. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
