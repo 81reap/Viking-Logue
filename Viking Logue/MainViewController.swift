75df4efbdcdb2a0b6fd0a208052c31578f56cede
Hax0rDoge <p.bhakar.work@gmail.com>
2017-01-16 17:08:24 -0600

    Added proper licences

---
+++ Viking Logue/MainViewController.swift
@@ -0,0 +1,9 @@
+//
+//  MainViewController.swift
+//  Viking Logue
+//
+//  Created by Prayag Bhakar on 1/16/17.
+//  Copyright © 2017 Prayag Bhakar. All rights reserved.
+//
+
+import Foundation
---
+++ Viking Logue/MenuTableViewController.swift
@@ -0,0 +1,9 @@
+//
+//  MenuTableViewController.swift
+//  Viking Logue
+//
+//  Created by Prayag Bhakar on 1/16/17.
+//  Copyright © 2017 Prayag Bhakar. All rights reserved.
+//
+
+import Foundation
