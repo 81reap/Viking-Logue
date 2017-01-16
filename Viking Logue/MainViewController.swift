23affff183cfc444b9d92c3c9b081aa914985065
Hax0rDoge <p.bhakar.work@gmail.com>
2017-01-16 17:08:57 -0600

    added functioning menu

--- Viking Logue/MainViewController.swift
+++ Viking Logue/MainViewController.swift
@@ -7,3 +7,31 @@
 //
 
 import Foundation
+
+class MainViewController: UITableViewController {
+    
+    @IBOutlet weak var Open: UIBarButtonItem!
+    @IBOutlet weak var navBar: UINavigationItem!
+    
+    var viewNum = Int()
+    var menuItems =  ["All","News","Features","A&E","Sports","Forum","Viking Voice","Humans of Fremd"]
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+        
+        Open.target = self.revealViewController()
+        Open.action = #selector(SWRevealViewController.revealToggle(_:))
+        
+        self.view.addGestureRecognizer(self.revealViewController().panGestureRecognizer())
+        
+        self.title = menuItems[0]
+    }
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    
+}
--- Viking Logue/MenuTableViewController.swift
+++ Viking Logue/MenuTableViewController.swift
@@ -7,3 +7,33 @@
 //
 
 import Foundation
+
+class MenuTableViewController: UITableViewController {
+    
+    var menuItems = [String]()
+    
+    override func viewDidLoad() {
+        menuItems = ["All","News","Features","A&E","Sports","Forum","Viking Voice","Humans of Fremd"]
+    }
+    
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return menuItems.count
+    }
+    
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "menuCell", for: indexPath) as UITableViewCell
+        
+        cell.textLabel?.text = menuItems[indexPath.row]
+        
+        return cell
+    }
+    
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        var endViewController = segue.destination as! MainViewController
+        
+        var indexPath : NSIndexPath = self.tableView.indexPathForSelectedRow! as NSIndexPath
+        
+        endViewController.viewNum = indexPath.row
+    }
+    
+}
